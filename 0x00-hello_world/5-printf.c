70d9c27c325d6db7e117413bdff803b6379542cb
Aimranerose26 <aimrane26@hotmail.com>
2023-07-13 05:22:19 -0700

    uiehf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of my program 
+ *
+ * Description : print the message "with proper grammar, but the outcome is a piece of art," with the printf funcction.
+ *
+ * Return : always 0 (Success)
+ */ 
+int main(){
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+
+return 0;
+}
