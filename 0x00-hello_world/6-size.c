bc1e3ceb0302fd328c021add6674f90a5c9b811d
Aimranerose26 <aimrane26@hotmail.com>
2023-07-13 05:49:36 -0700

    irhf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Entry point :the place where i start my program
+ *
+ * Description : writing a program that prints the size of various types on the computer
+ * 
+ * Return 0 (Success)
+ */
+int main(){
+	printf("the size of a char is :%zu byte(s)\n",sizeof(char));
+		printf("the size of an int is :%zu byte(s)\n",sizeof(int));
+		printf("the size of a long int is :%zu byte(s)\n",sizeof(long int));
+		printf("the size of a long long int is :%zu byte(s)\n",sizeof(long long int));
+		printf("the size of a float is:%zu byte(s)",sizeof(float));
+		printf("the size of a double is:%zu byte(s)\n",sizeof(double));
+		return 0;
+}
