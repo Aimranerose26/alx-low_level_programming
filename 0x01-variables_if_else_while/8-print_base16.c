41dadae26055e97dec48b13f8f02ae866f8a601e
Aimranerose26 <aimrane26@hotmail.com>
2023-07-14 16:50:12 -0700

    ienfo

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: (0) Success
+*/
+int main(void)
+{
+	int x;
+	char alp;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		putchar('0' + x);
+	}
+	for (alp = 'a' ; alp <= 'f' ; alp++)
+	{
+		putchar(alp);
+	}
+	return (0);
+}
