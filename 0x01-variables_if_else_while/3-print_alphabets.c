497f7840fa2bf0f6f6307be5c8d034449490160c
Aimranerose26 <aimrane26@hotmail.com>
2023-07-14 15:12:40 -0700

    uebf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - entery
+*Description: prints alphabets in lower case
+*Return:Always if successful
+*/
+int main(void)
+{
+	char alphabet;
+	char ALPHABET;
+
+	alphabet = 'a';
+	ALPHABET = 'A';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	while (ALPHABET<='Z')
+	{
+		putchar(ALPHABET);
+		ALPHABET++;
+	}
+	putchar('\n');
+
+	return (0);
+}
