b187bede08f657a6ae9e9bc6b1713797c23b44c9
Aimranerose26 <aimrane26@hotmail.com>
2023-07-14 11:24:17 -0700

    aurh

---
+++ 0x01-variables_if_else_while/2-print_alphab.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - entery
+*Description: prints alphabets in lower case
+*Return: (0) if successful
+*/
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
