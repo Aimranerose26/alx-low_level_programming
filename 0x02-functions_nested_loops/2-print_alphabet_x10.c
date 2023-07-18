2be065863056edc98beb80efdf4591b9c2d01d51
Aimranerose26 <aimrane26@hotmail.com>
2023-07-18 10:09:52 -0700

    ainfp

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *Description: writing function that prints 10 times the alphabet.
+ *Return: (0) success
+ */
+
+int main(void)
+{
+	int alph, or;
+
+	alph = 0;
+
+	while (alph < 10)
+	{
+		alph++
+			for (or = 'a' ; or <= 'z' ; or++)
+			{
+				_putchar(or);
+			}
+		or++
+		_putchar('\n');
+	}
+}
