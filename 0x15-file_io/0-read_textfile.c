3cdaf202b2624a3af095e904292f0d1d1618f484
Aimranerose26 <aimrane26@hotmail.com>
2023-09-04 09:08:02 -0700

    cocomel

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,49 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints it
- * to the POSIX standard output
- * @filename: name of the file
- * @letters: number of letters it should read and print
- * Return: actual number of letters it could read and print
+ *read_textfile - a func that read a text from a file
+ *@filename: a ptr to the file name
+ *@letters: the number of letter nedds to be readed
+ *Return: 0 if fails,, otherwise return the number of numbers readed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, num_to_print, written_num;
-	char *buffer;
+	ssize_t counter = 0, rest, read_flag = 0;
+	size_t  lenght;
+	int fd;
+	char *buf;
 
+	rest = letters >= BUFF_SIZE ? BUFF_SIZE : letters;
 	if (!filename)
 		return (0);
-
+	buf = malloc(BUFF_SIZE * sizeof(char));
+	if (!buf)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	buffer = malloc(letters * sizeof(char));
-	if (!buffer)
+	do {
+	if (rest < BUFF_SIZE)
+	{
+		read_flag = read(fd, buf, rest);
+	}
+	else
+		read_flag = read(fd, buf, sizeof(buf));
+	if (read_flag == -1)
+	{
+		close(fd);
 		return (0);
-
-	/* return Number of bytes read on success */
-	num_to_print = read(fd, buffer, letters);
-	/* return Number of bytes written on success */
-	written_num = write(STDOUT_FILENO, buffer, num_to_print);
-
-	if (written_num < 0)
+	}
+	lenght = (size_t) read_flag;
+	counter += lenght;
+	rest = letters - counter;
+	if (write(STDOUT_FILENO, buf, lenght) == -1)
+	{
+		close(fd);
 		return (0);
-
-	free(buffer);
+	}
+	} while (((size_t) counter) < letters && lenght > 0);
 	close(fd);
-	return (num_to_print);
+	return (counter);
 }
